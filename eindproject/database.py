038e6a0c245f004e908d164c650bc1046cbad844
Aleksander Baum <102529860+AleksanderBaum@users.noreply.github.com>
2024-01-02 21:17:14 +0100

    sql changes

--- eindproject/database.py
+++ eindproject/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:abc123!@localhost/restaurantdb"
+SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:abc123!@mysql/restaurantdb"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, echo=True
